a62531d02a32ce054fca07fbadf9ea86a12840c0
bilalsattar24 <bilalsattar24@gmail.com>
2017-08-17 22:21:33 -0700

    hello world functioning

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.getElementById("mainButton").onclick = test;
+
+function test() {
     console.log("hello world");
-})
+    document.getElementById("h1").text("testing");
+}

6c3d8e5a242e529db2129cb3eb2f54e0e07a0dcb
bilalsattar24 <bilalsattar24@gmail.com>
2017-10-19 23:21:49 -0700

    adds text "TEST" to my team page

--- popup.js
+++ popup.js
@@ -1,9 +1,15 @@
-document.getElementById("mainButton").onclick = test;
-var url = window.location.href;
-print(url);
 function test() {
     //need to access current tab and change something. 
+    var url = window.location.href;
+    var regexTeam = "/https?:\/\/basketball[.]fantasysports[.]yahoo[.]com\/nba\/\d{3,7}\/\d{1,2}/";
+    var urlMatch = url.match(regexTeam);
+    var div = document.createElement('div');
+    div.innerHTML = "<h1>TEST</h1>";
+    //document.getElementById("team-card-info").appendChild(div);
+    document.getElementsByClassName("Grid-u Px-med Fz-sm Va-mid")[0].appendChild(div);
+    document.getElementsByClassName("Grid-u Px-med Fz-sm Va-mid")[0].style.display = '-webkit-inline-box';
     console.log("hello world");
-    document.getElementById("h1").text("testing");
-    document.getElementById("gb_P").innerHTML("ugly");
 }
+var url = window.location.href;
+console.log(url);
+test()

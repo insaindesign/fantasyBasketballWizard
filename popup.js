f0d6a91fcd52d568dcabc346b4ee0cf264e8ec5d
bilalsattar24 <bilalsattar24@gmail.com>
2017-10-19 19:49:08 -0700

    2d array

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 document.getElementById("mainButton").onclick = test;
-
+var url = window.location.href;
+print(url);
 function test() {
+    //need to access current tab and change something. 
     console.log("hello world");
     document.getElementById("h1").text("testing");
+    document.getElementById("gb_P").innerHTML("ugly");
 }

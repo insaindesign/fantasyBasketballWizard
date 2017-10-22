8f3e3774bb85dbc5cf5fab208a513acbbc78de18
bilalsattar24 <bilalsattar24@gmail.com>
2017-10-21 22:30:07 -0700

    clean up

--- popup.js
+++ popup.js
@@ -79,16 +79,8 @@ var fantasyWeek = week - 42 - 1;
 console.log(fantasyWeek);
 
 function render() {
-
-    var url = window.location.href;
-    var regexTeam = "/https?:\/\/basketball[.]fantasysports[.]yahoo[.]com\/nba\/\d{3,7}\/\d{1,2}/";
-    var urlMatch = url.match(regexTeam);
-    var players = {};
-    var table_opp;
     var info;
-    
-    
-    
+
     //init stats table
     table = document.getElementById("statTable0");
     
@@ -112,76 +104,24 @@ function render() {
 
     //replace Fantasy headers
     table.rows[0].cells[6].innerText = "Games";
-    //table.rows[1].cells[6].innerText = "Remaining";
+    //table.rows[1].cells[6].innerText = "Remaining"; coming soon ;)
     table.rows[1].cells[8].innerText = "Total";
     
     //determine games and replace fantasy col
     row = 2;
+    totalGames = 0;
     info = table.rows[row].cells[player_col].innerText.split(" ");
     while (info.length > 3){
         console.log(info)
         table.rows[row].cells[fantasy_col].innerText = Schedule[info[info.length - 3]][fantasyWeek];
+        totalGames += Schedule[info[info.length - 3]][fantasyWeek];
         row++;
         info = table.rows[row].cells[player_col].innerText.split(" ");
         console.log(table.rows[row]);
     }
     
+    console.log("total games" + totalGames);
     console.log(table.rows[row]);
-    console.log(info);
-    
-    /*for (var i = 2; i < num_rows - 3; i++){
-        info = table.rows[i].cells[player_col].innerText.split(" ")
-        console.log(info)
-        table.rows[i].cells[fantasy_col].innerText = Schedule[info[info.length - 3]][fantasyWeek]
-        
-    }*/
-    
-    /*//create opponent page url
-    url_post = "?date="
-    date = new Date()
-    year = date.getYear() + 1900;
-    month = date.getMonth() + 1;
-    date.getDate() - date.getDay() + 1;
-    day = date.getDay()
-    url_post = url_post + year + "-" + month + "-" + day + "&stat1=O";
-    url_full = url + url_post;
-    
-    
-    
-    //request opponent page
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url_full, true);
-    console.log(url_full)
-    xhr.responseType = 'document';
-    xhr.send();
-    xhr.onload = function(e, write) {
-        
-        //download opponent doc and table
-        doc = e.target.responseXML;
-        table_opp = doc.getElementById("statTable0")
-        
-    }
-    
-    write = function(){
-        for(row = 2; row < num_rows - 5; row++){
-            var rem = 0;
-            var total = 0;
-            for (col = 6; col < 13; col++){
-                console.log(table_opp.rows[row].cells[col].innerText)
-                var hasGame = Boolean(table_opp.rows[row].cells[col].innerText) ? 1 : 0;
-                total = total + hasGame;
-                if (col - 6 > day)
-                    rem = rem + hasGame;
-            }
-            //write to cells
-            console.log("write")
-            table.rows[row].cells[6].innerText = rem
-            table.rows[row].cells[7].innerText = total
-        }
-        
-
-    }*/
-
+    console.log(info); 
 }
-var url = window.location.href;
-render()
+render();

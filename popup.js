ae37c091fc2ccf16fe2addfcf1aceb1bfde429e3
alexanderDoria <adoria@csumb.edu>
2017-10-21 19:46:39 -0700

    total games calculated

--- popup.js
+++ popup.js
@@ -1,10 +1,91 @@
+
+
+var Schedule = {}
+
+AtlantaHawks = [3,4,3,3,3,4,2,4,3,4,3,3,3,3,4,4,4,3,4,3,3,4,3,4,1]
+BostonCeltics = [3,3,4,4,3,4,3,4,4,4,4,3,1,3,3,4,4,3,3,3,3,3,3,4,2]
+BrooklynNets = [3,4,2,4,3,3,3,3,4,3,4,3,4,4,3,4,3,3,4,3,3,4,3,3,2]
+CharlotteHornets = [2,4,4,2,3,4,2,4,4,4,3,2,3,3,4,4,4,4,4,3,3,4,4,3,1]
+ChicagoBulls = [2,3,3,3,3,4,3,4,3,4,4,4,3,3,4,2,3,4,3,4,3,4,4,3,2]
+ClevelandCavaliers = [3,4,3,3,3,3,4,4,4,2,3,3,3,3,3,3,4,4,3,4,3,4,4,3,2]
+DallasMavericks = [3,4,4,3,3,3,3,4,3,4,4,3,3,2,4,3,4,3,4,3,3,3,4,4,1]
+DenverNuggets = [2,4,4,3,3,3,3,4,3,4,3,3,4,3,3,4,3,4,3,4,3,3,4,3,2]
+DetroitPistons = [3,4,3,3,3,2,4,4,4,2,3,3,3,4,3,3,4,4,4,3,3,4,4,3,2]
+GoldenStateWarriors = [3,4,3,3,4,3,4,3,2,4,4,3,4,3,3,3,3,4,3,4,3,3,4,4,1]
+HoustonRockets = [3,4,4,3,3,2,3,2,4,3,4,3,3,3,4,3,4,4,3,4,4,4,3,3,2]
+IndianaPacers = [3,3,4,4,3,3,3,4,3,3,4,2,4,4,3,4,4,2,4,4,3,3,3,4,1]
+LosAngelesClippers = [2,3,4,3,3,3,4,2,4,4,3,3,4,3,4,2,3,4,4,3,4,4,4,3,2]
+LosAngelesLakers = [3,3,4,3,4,2,4,2,2,4,4,4,3,4,3,3,3,4,3,4,3,3,4,4,2]
+MemphisGrizzlies = [2,4,4,2,3,4,3,4,4,3,4,2,2,4,3,4,3,3,4,4,3,4,4,3,2]
+MiamiHeat = [2,3,4,4,3,3,4,2,4,4,3,3,3,4,3,4,3,4,3,4,3,4,3,3,2]
+MilwaukeeBucks = [3,3,3,3,3,3,3,4,3,3,3,4,4,3,3,4,3,4,4,3,3,4,4,3,2]
+MinnesotaTimberwolves = [3,3,4,2,4,4,4,3,3,3,4,4,4,3,4,4,3,4,3,2,3,3,4,2,2]
+NewOrleansPelicans = [3,3,4,3,3,4,3,4,3,3,3,2,4,3,4,3,4,4,3,4,4,3,3,3,2]
+NewYorkKnicks = [2,3,4,3,3,4,3,4,3,3,4,4,3,4,3,4,3,4,3,3,3,4,3,3,2]
+OklahomaCityThunder = [3,3,3,4,2,4,3,3,4,4,4,3,3,3,4,4,3,4,4,3,4,3,3,2,2]
+OrlandoMagic = [3,3,4,3,3,4,4,4,3,3,3,3,3,3,2,3,4,4,4,4,3,3,3,4,2]
+Philadelphia76ers = [3,3,3,3,3,3,4,4,2,4,4,2,1,3,4,4,3,5,4,3,3,4,4,4,2]
+PhoenixSuns = [3,3,4,4,4,3,3,4,3,4,3,4,2,2,4,4,3,4,4,3,3,3,4,3,1]
+PortlandTrailBlazers = [3,3,4,2,4,4,3,2,4,4,2,4,4,3,3,4,4,3,3,3,4,3,4,3,2]
+SacramentoKings = [3,3,3,3,4,3,4,3,3,3,4,2,4,3,4,3,3,4,5,3,4,3,4,2,2]
+SanAntonioSpurs = [2,4,4,3,3,3,4,4,3,4,3,4,3,4,4,3,2,4,2,3,4,4,3,3,2]
+TorontoRaptors = [2,3,4,3,4,3,2,3,4,3,3,3,4,4,3,4,3,3,4,4,4,4,2,4,2]
+UtahJazz = [3,3,4,3,4,3,3,4,3,4,3,3,2,4,3,3,4,4,3,4,3,4,3,3,2]
+WashingtonWizards = [2,4,3,3,4,3,3,4,4,3,4,3,3,3,3,3,4,4,4,3,3,3,4,3,2]
+
+Schedule["Atl"] = AtlantaHawks
+Schedule["Bos"] = BostonCeltics
+Schedule["Bkn"] = BrooklynNets
+Schedule["Cha"] = CharlotteHornets
+Schedule["Chi"] = ChicagoBulls
+Schedule["Cle"] = ClevelandCavaliers
+Schedule["Dal"] = DallasMavericks
+Schedule["Den"] = DenverNuggets
+Schedule["Det"] = DetroitPistons
+Schedule["GS"] = GoldenStateWarriors
+Schedule["Hou"] = HoustonRockets
+Schedule["Ind"] = IndianaPacers
+Schedule["LAC"] = LosAngelesClippers
+Schedule["LAL"] = LosAngelesLakers
+Schedule["Mem"] = MemphisGrizzlies
+Schedule["Mia"] = MiamiHeat
+Schedule["Mil"] = MilwaukeeBucks
+Schedule["Min"] = MinnesotaTimberwolves
+Schedule["NO"] = NewOrleansPelicans
+Schedule["NYK"] = NewYorkKnicks
+Schedule["OKC"] = OklahomaCityThunder
+Schedule["Orl"] = OrlandoMagic
+Schedule["Phi"] = Philadelphia76ers
+Schedule["Pho"] = PhoenixSuns
+Schedule["Por"] = PortlandTrailBlazers
+Schedule["Sac"] = SacramentoKings
+Schedule["SA"] = SanAntonioSpurs
+Schedule["Tor"] = TorontoRaptors
+Schedule["UTA"] = UtahJazz
+Schedule["Was"] = WashingtonWizards
+
+
+//get week of year
+Date.prototype.getWeek = function() {
+    var onejan = new Date(this.getFullYear(), 0, 1);
+        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+    }
+
+var week = (new Date()).getWeek();
+
+//Handles matchup week
+var fantasyWeek = week - 42 - 1;
+
+console.log(fantasyWeek);
+
 function render() {
 
     var url = window.location.href;
     var regexTeam = "/https?:\/\/basketball[.]fantasysports[.]yahoo[.]com\/nba\/\d{3,7}\/\d{1,2}/";
     var urlMatch = url.match(regexTeam);
-    var players = {}
+    var players = {};
     var table_opp;
+    var info;
     
     
     //init stats table
@@ -15,12 +96,18 @@ function render() {
     num_rows = table.rows.length;
 
     //replace Rankings headers
-    table.rows[0].cells[5].innerText = "Games";
-    table.rows[1].cells[6].innerText = "Remaining";
-    table.rows[1].cells[7].innerText = "Total";
+    table.rows[0].cells[6].innerText = "Games";
+    //table.rows[1].cells[6].innerText = "Remaining";
+    table.rows[1].cells[8].innerText = "Total";
     
     
-    //create opponent page url
+    for (var i = 2; i < num_rows - 3; i++){
+        info = table.rows[i].cells[2].innerText.split(" ")
+        table.rows[i].cells[8].innerText = Schedule[info[info.length - 3]][fantasyWeek]
+        
+    }
+    
+    /*//create opponent page url
     url_post = "?date="
     date = new Date()
     year = date.getYear() + 1900;
@@ -64,7 +151,7 @@ function render() {
         }
         
 
-    }
+    }*/
 
 }
 var url = window.location.href;

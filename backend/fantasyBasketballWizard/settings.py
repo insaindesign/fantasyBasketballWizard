89da76d59038efa02a0658bfa1dc031f29689356
bilalsattar24 <bilalsattar24@gmail.com>
2018-09-05 22:54:12 -0700

    add allowed host python anywhere

--- backend/fantasyBasketballWizard/settings.py
+++ backend/fantasyBasketballWizard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dpz9@_x5lgpbdypb4i=yi%kcq&cb0e!)=$$3gct#@$c%#1#9p0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bilalsattar24.pythonanywhere.com']
 
 
 # Application definition

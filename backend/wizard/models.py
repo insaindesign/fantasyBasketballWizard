63a5bd035fb7396b95a5b73453a7479ad211ca40
bilalsattar24 <bilalsattar24@gmail.com>
2018-09-23 11:06:35 -0700

    updates to player model to store stats

--- backend/wizard/models.py
+++ backend/wizard/models.py
@@ -14,7 +14,7 @@ class YahooUseType(models.Model):
     pageName = models.CharField(max_length=25, primary_key=True)
 
     def __str__(self):
-        return self.pk
+        return str(self.pageName)
 
 #if we wanna keep track of Yahoo emails   
 class YahooUser(models.Model):
@@ -25,10 +25,9 @@ class YahooUser(models.Model):
 class YahooUse(models.Model):
     timeStamp = models.DateTimeField(auto_now=True)
     useType = models.ForeignKey(YahooUseType,on_delete=models.CASCADE)
-    user = models.ForeignKey(YahooUser,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.useType
+        return str(self.useType)
 
 
 class ESPNUse(models.Model):
@@ -72,23 +71,24 @@ class Game(models.Model):
         return str(self.roadTeam) + " @ " + str(self.homeTeam) + " - " + str(self.time) + " " + str(self.date)
 
 class Player(models.Model):
-    yahooPlayerID = models.IntegerField(primary_key=True)
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
+    playerID = models.CharField(max_length=60)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    ppg = models.DecimalField(max_digits=2, decimal_places=1)
-    rpg = models.DecimalField(max_digits=2, decimal_places=1)
-    apg = models.DecimalField(max_digits=2, decimal_places=1)
-    spg = models.DecimalField(max_digits=1, decimal_places=1)
-    bpg = models.DecimalField(max_digits=1, decimal_places=1)
-    topg = models.DecimalField(max_digits=1, decimal_places=1)
-    ftmpg = models.DecimalField(max_digits=2, decimal_places=1)
-    ftapg = models.DecimalField(max_digits=2, decimal_places=1)
-    ftpct = models.DecimalField(max_digits=2, decimal_places=1)
-    fgapg = models.DecimalField(max_digits=2, decimal_places=1)
-    fgmpg = models.DecimalField(max_digits=2, decimal_places=1)
-    fgpct = models.DecimalField(max_digits=2, decimal_places=1)
-    threepg = models.DecimalField(max_digits=1, decimal_places=1)
+    ppg = models.DecimalField(max_digits=3, decimal_places=1)
+    rpg = models.DecimalField(max_digits=3, decimal_places=1)
+    apg = models.DecimalField(max_digits=3, decimal_places=1)
+    spg = models.DecimalField(max_digits=2, decimal_places=1)
+    bpg = models.DecimalField(max_digits=2, decimal_places=1)
+    topg = models.DecimalField(max_digits=2, decimal_places=1)
+    ftmpg = models.DecimalField(max_digits=3, decimal_places=1)
+    ftapg = models.DecimalField(max_digits=3, decimal_places=1)
+    ftpct = models.DecimalField(max_digits=3, decimal_places=1)
+    fgapg = models.DecimalField(max_digits=3, decimal_places=1)
+    fgmpg = models.DecimalField(max_digits=3, decimal_places=1)
+    fgpct = models.DecimalField(max_digits=3, decimal_places=1)
+    threepg = models.DecimalField(max_digits=2, decimal_places=1)
+
+    def __str__(self):
+        return str(self.playerID)
 
 class Test(models.Model):
     test = models.CharField(max_length=100)

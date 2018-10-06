6e29cc7cd5c38dba0a61ea36af4918fb2b1d0b31
lawjonathan17 <lawjonathan17@gmail.com>
2018-10-06 11:35:33 -0700

    ESPN - matchups page

--- js/espn/matchupsPage.js
+++ js/espn/matchupsPage.js
@@ -49,12 +49,9 @@ function buildLeagueIdRequestString()
 {
     console.log( "buildLeagueIdRequestString()" );
     var entireUrl = window.location.href;
-    // console.log( url );
     var url = new URL( entireUrl );
     var leagueId = url.searchParams.get( "leagueId" );
-    console.log( leagueId );
-    var leagueIdRequestString = "leagueID="+leagueId;
-    console.log( leagueIdRequestString );
+    var leagueIdRequestString = "leagueID=" + leagueId;
     return leagueIdRequestString;
 }
 
@@ -70,15 +67,15 @@ function buildTeamsRequestString()
     var teamsRequestString = "teams=";
     for( var i = 0; i < listOfElements.length; i++ )
     {
-        if( !( acronymEspnToYahoo[ listOfElements[ i ].innerHTML ] in localGamesDataDict ) )
-        {
-            localGamesDataDict[ acronymEspnToYahoo[ listOfElements[ i ].innerHTML ] ] = "";
-            teamsRequestString += acronymEspnToYahoo[ listOfElements[ i ].innerHTML ] + ",";
-        }
+        teamsRequestString += acronymEspnToYahoo[ listOfElements[ i ].innerHTML ] + ",";
     }
     return teamsRequestString;
 }
 
+/*
+    buildDateRequestString - creates part of the url 
+    for the teams to request game data for.
+*/
 function buildDateRequestString()
 {
     console.log( "buildDateRequestString()" );
@@ -120,26 +117,54 @@ function buildDateRequestString()
     }
 }
 
+/* 
+    getBackgroundColor - returns the background color
+    associated with the number of games.
+
+    games - The number of games
+*/
+function getBackgroundColor( games )
+{
+    if( games > 3 )
+    {
+        return "#adebad";
+    }
+    else if( games == 3 )
+    {
+        return "#d8ffcc";
+    }
+    else if( games == 2 )
+    {
+        return "#ffffcc";
+    }
+    else if( games == 1 )
+    {
+        return "#ffe1ba";
+    }
+    else if( games == 0 )
+    {
+        return "#ffd6cc";
+    }
+}
+
+
+/* ---------------------------------------------------------------------
+                            Main Functions 
+--------------------------------------------------------------------- */
 
 /*
     addGamesDataToLocalDictionary - 
 */
 function addGamesDataToLocalDictionary( data, teamsRequestString )
 {
-    // Don't need to erase the local games info because the date doesn't change for free agents, can build onto
-    // The data structure as I use it
-    // localGamesDataDict = {};
     console.log( "addDataToLocalDictionary()" );
+    localGamesDataDict = {};
     var teamsRequestStringConcise = teamsRequestString.substring( 6, teamsRequestString.length-1 );
     var teamsList = teamsRequestStringConcise.split( "," );
     for( var i = 0; i < data.length; i++ )
     {
-        if( localGamesDataDict[teamsList[i]] == "" )
-        {
-            localGamesDataDict[teamsList[i]] = data[i];
-        }
+        localGamesDataDict[teamsList[i]] = data[i];
     }
-    console.log( localGamesDataDict );
 }
 
 /*
@@ -181,13 +206,160 @@ function requestGameDataFromServer()
 function addGamesForPlayers()
 {
     console.log( "addGamesForPlayers()" );
+
+    var listOfElements = document.getElementsByClassName( "Table2__tr" );
+    var leftTotalGamesRemaining = 0;
+    var leftTotalGamesForWeek = 0;
+    var rightTotalGamesRemaining = 0;
+    var rightTotalGamesForWeek = 0;
+
+    for( var i = 0; i < listOfElements.length; i++ )
+    {
+        var listOfElementsChildren = listOfElements[i].children;
+        var playerColumnLeft = listOfElementsChildren[0];
+        var playerColumnLeftScore = listOfElementsChildren[1];
+        var playerColumnRight = listOfElementsChildren[4];
+        var playerColumnRightScore = listOfElementsChildren[3];
+        var newLeftGamesDiv = document.createElement( "div" );
+        var newRightGamesDiv = document.createElement( "div" );
+        newLeftGamesDiv.className = "jsx-2810852873 table--cell points-column fbw-new-element";
+        newRightGamesDiv.className = "jsx-2810852873 table--cell points-column reverse fbw-new-element";
+
+        // Total Row
+        if( listOfElements[i].className.indexOf( "total-row" ) != -1 )
+        {
+            var leftTotalGamesString = leftTotalGamesRemaining.toString() + "/" + leftTotalGamesForWeek.toString();
+            var rightTotalGamesString = rightTotalGamesRemaining.toString() + "/" + rightTotalGamesForWeek.toString();
+            newLeftGamesDiv.className += " fbw-left-total";
+            newRightGamesDiv.className += " fbw-right-total";
+            newLeftGamesDiv.innerHTML = leftTotalGamesString;
+            newRightGamesDiv.innerHTML = rightTotalGamesString;
+
+            if( leftTotalGamesRemaining > rightTotalGamesRemaining )
+            {
+                newLeftGamesDiv.style.color = "#acd888";
+            }
+            else if( rightTotalGamesRemaining > leftTotalGamesRemaining )
+            {
+                newRightGamesDiv.style.color = "#acd888";
+            }
+            else if( leftTotalGamesRemaining == rightTotalGamesRemaining ){}
+        }
+        // Player Row
+        else
+        {
+            // Check if the player is injured
+            var isLeftInjured = false;
+            var isRightInjured = false;
+            var leftInjuredElements = playerColumnLeft.getElementsByClassName( "injury-status_medium" );
+            var rightInjuredElements = playerColumnRight.getElementsByClassName( "injury-status_medium" );
+
+            if( leftInjuredElements.length > 0 )
+            {
+                if( leftInjuredElements[0].innerHTML == "O" )
+                {
+                    isLeftInjured = true;
+                }
+            }
+
+            if( rightInjuredElements.length > 0 )
+            {
+                if( rightInjuredElements[0].innerHTML == "O" )
+                {
+                    isRightInjured = true;
+                }
+            }
+
+            // Empty Left Player
+            if( playerColumnLeft.innerHTML.indexOf( "</div>Empty</div>" ) != -1 )
+            {
+                newLeftGamesDiv.innerHTML = "-/-";
+                newLeftGamesDiv.style.backgroundColor = getBackgroundColor( 0 );
+            }
+            // Normal Left Player
+            else
+            {
+                if( !isLeftInjured )
+                {
+                    var teamNameElements = playerColumnLeft.getElementsByClassName( "playerinfo__playerteam" );
+                    var teamName = acronymEspnToYahoo[teamNameElements[0].innerHTML];
+                    newLeftGamesDiv.innerHTML = localGamesDataDict[teamName];
+                    var splitDataIndex = localGamesDataDict[teamName].split( "/" );
+                    leftTotalGamesRemaining += parseInt( splitDataIndex[0] );
+                    leftTotalGamesForWeek += parseInt( splitDataIndex[1] );
+                    newLeftGamesDiv.style.backgroundColor = getBackgroundColor( splitDataIndex[0] );
+                }
+                else
+                {
+                    newLeftGamesDiv.innerHTML = "-/-";
+                    newLeftGamesDiv.style.backgroundColor = getBackgroundColor( 0 );
+                }
+            }
+            // Empty Right Player
+            if( playerColumnRight.innerHTML.indexOf( "</div>Empty</div>" ) != -1 )
+            {
+                newRightGamesDiv.innerHTML = "-/-";
+                newRightGamesDiv.style.backgroundColor = getBackgroundColor( 0 );
+            }
+            // Normal Right Player
+            else
+            {
+                if( !isRightInjured )
+                {
+                    var teamNameElements = playerColumnRight.getElementsByClassName( "playerinfo__playerteam" );
+                    var teamName = acronymEspnToYahoo[teamNameElements[0].innerHTML];
+                    newRightGamesDiv.innerHTML = localGamesDataDict[teamName];
+                    var splitDataIndex = localGamesDataDict[teamName].split( "/" );
+                    rightTotalGamesRemaining += parseInt( splitDataIndex[0] );
+                    rightTotalGamesForWeek += parseInt( splitDataIndex[1] );
+                    newRightGamesDiv.style.backgroundColor = getBackgroundColor( splitDataIndex[0] ); 
+                }
+                else
+                {
+                    newRightGamesDiv.innerHTML = "-/-";
+                    newRightGamesDiv.style.backgroundColor = getBackgroundColor( 0 );
+                }
+            }
+        }
+        playerColumnLeftScore.appendChild( newLeftGamesDiv );
+        playerColumnRightScore.appendChild( newRightGamesDiv );
+    }
+
+    // Update total games
+    var leftTotalElements = document.getElementsByClassName( "fbw-left-total" );
+    var rightTotalElements = document.getElementsByClassName( "fbw-right-total" );
+
+    if( leftTotalElements.length > 0 )
+    {
+        var leftTotalGamesString = leftTotalGamesRemaining.toString() + "/" + leftTotalGamesForWeek.toString();
+        var rightTotalGamesString = rightTotalGamesRemaining.toString() + "/" + rightTotalGamesForWeek.toString();
+        leftTotalElements[0].innerHTML = leftTotalGamesString;
+        rightTotalElements[0].innerHTML = rightTotalGamesString;
+
+    }
 }
 
-$( 'body' ).on( 'click', 'select.dropdown__select', function() 
+/*
+    removeEntireColumn - removes the games data column to allow new data to be filled
+*/
+function removeEntireColumn()
+{
+    console.log( "removeEntireColumn" );
+    var elements = document.getElementsByClassName( "fbw-new-element" );
+
+    while( elements.length > 0 )
+    {
+        elements[0].parentNode.removeChild( elements[0] );
+        console.log( "deleted an element" );
+    }
+}
+
+$( 'body' ).on( 'change', 'select.dropdown__select', function() 
 {
     // console.log( $(".dropdown__select option:selected").text() );
-    console.log( "option clicked" );
+    console.log( "option changed" );
     // getDate();
+    removeEntireColumn();
     requestGameDataFromServer();
 });
 
@@ -195,10 +367,8 @@ $( document ).ready( function()
 {
     setTimeout( function()
     {
-        buildDateRequestString();
         // var documentReadyDate = $(".dropdown__select option:selected").text();
         // console.log( "documentReadyDate=" + documentReadyDate );
         requestGameDataFromServer();
     }, 5000 );
 });
-

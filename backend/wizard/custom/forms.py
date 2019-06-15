567c7c9306f0a8aaf23bea32a8ad1dcbbd5583d3
bilalsattar24 <bilalsattar24@gmail.com>
2019-06-15 16:54:09 -0700

    add custom registration form with email required

---
+++ backend/wizard/custom/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.db import models
+
+class RegistrationForm(UserCreationForm):
+    
+    email = forms.EmailField(max_length=254, help_text='Must be a valid address')
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'password1',
+            'password2',
+        )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user

585d8087bf58f9445f92c84a02886068b22f5572
bilalsattar24 <bilalsattar24@gmail.com>
2019-10-11 22:56:26 -0700

    background accepts parameters instead of url

--- js/background.js
+++ js/background.js
@@ -10,19 +10,22 @@ chrome.runtime.onMessage.addListener(function(
   sendResponse,
   callback
 ) {
-  let url = request.url;
+  let { endpoint, date, week, teams, pageName, leagueID } = request;
+  let domain = "https://www.sportswzrd.com";
+  let url = `${domain}/${endpoint}?format=json&teams=${teams}&date=${date}&pageName=${pageName}&leagueID=${leagueID}`;
+  console.log(url);
   fetch(url)
     .then(function(response) {
       if (response.status !== 200) {
         console.log("response status: " + response.status);
-        return;
+        return false;
       }
       response.json().then(function(data) {
         sendResponse({ data: data });
       });
     })
     .catch(function(err) {
-      console.log("EXCEPTION", err);
+      console.log("fetch failed", err);
       return false;
     });
   return true;

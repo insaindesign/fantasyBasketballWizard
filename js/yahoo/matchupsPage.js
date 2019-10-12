15574dd3fa71fd6b4459893167724459ed38df38
bilalsattar24 <bilalsattar24@gmail.com>
2019-10-12 00:39:09 -0700

    remove games today stuff

--- js/yahoo/matchupsPage.js
+++ js/yahoo/matchupsPage.js
@@ -313,34 +313,6 @@ function initTable() {
   matchup.appendChild(pTable);
 }
 
-function getFormattedQueryFromURL() {
-  var url = window.location.href;
-  var start = url.indexOf("date");
-
-  var end = url.indexOf("&", start);
-
-  var str = url.substring(start + 5, end);
-  if (str == "totals" || start == -1) {
-    var weekNum = "weekNum=";
-    weekNum += document
-      .getElementsByClassName("flyout-title")[0]
-      .innerText.split(" ")[1];
-    //console.log(weekNum);
-    return weekNum;
-  }
-  return "date=" + str;
-}
-
-function getGamesToday() {
-  var query = getFormattedQueryFromURL();
-  chrome.runtime.sendMessage({ endpoint: "gamestoday", query: query }, function(
-    response
-  ) {
-    var data = response.data;
-    displayGamesToday(data);
-  });
-}
-
 function serializePlayer(p) {
   p = p.replace(/ /g, "");
   p = p.replace("-", "");
@@ -570,19 +542,6 @@ function calculateStats(data, cat) {
   }
 }
 
-function displayGamesToday(data) {
-  var div = document.createElement("p");
-  div.innerText = " NBA games today: " + data;
-  div.setAttribute("class", "Ta-c C-grey Mt-10");
-  div.style.color = "#0078FF";
-  div.style.fontSize = "13px";
-  div.style.marginTop = "0px";
-  var className =
-    "felo-matchup-button yfa-rapid-beacon yfa-rapid-module-felo-matchup-button F-link Cur-p Fz-m";
-  var parentDiv = document.getElementsByClassName(className)[0].parentNode;
-  parentDiv.appendChild(div);
-}
-
 if (
   categories
     .toString()
@@ -592,10 +551,4 @@ if (
   initTable();
   //console.log("9cat league");
   getGamesRemaining(teams);
-}
-
-try {
-  getGamesToday();
-} catch (err) {
-  //console.log("error displaying games today");
 }
